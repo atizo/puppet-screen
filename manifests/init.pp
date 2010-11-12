ac93c28f8207fda7e4bc7bf7ee553feae2eebce4
Simon Josi <simon.josi@atizo.com>
2010-11-12 22:28:59 +0100

    cosmetics

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,9 @@
 #
 # Copyright 2008, admin(at)immerda.ch
 # Copyright 2008, Puzzle ITC GmbH
+# Copyright 2010, Atizo AG
 # Marcel Härry haerry+puppet(at)puzzle.ch
-# Simon Josi josi+puppet(at)puzzle.ch
+# Simon Josi simon.josi+puppet(at)atizo.com
 #
 # This program is free software; you can redistribute 
 # it and/or modify it under the terms of the GNU 
@@ -13,10 +14,10 @@
 #
 
 class screen {
-    case $operatingsystem {
-        openbsd: { include screen::openbsd }
-        gentoo: { include screen::gentoo }
-        default: { include screen::base }
-    }
-    screen::screenrc{ "root_screenrc": }
+  case $operatingsystem {
+    openbsd: { include screen::openbsd }
+    gentoo:  { include screen::gentoo }
+    default: { include screen::base }
+  }
+  screen::screenrc{'root_screenrc': }
 }
--- manifests/screenrc.pp
+++ manifests/screenrc.pp
@@ -3,10 +3,10 @@ define screen::screenrc(
   $target = '/root/.screenrc',
   $owner  = 'root',
   $group  = '0'
-){
-  file {$name:
+) {
+  file{$name:
     path => $target,
-    source => "puppet://$server/modules/screen/${source}",
+    source => "puppet://$server/modules/screen/$source",
     owner => $owner, group => $group, mode => 0600;
   }
 }
